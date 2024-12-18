82f4c7d9a561d82fd6d89fc52a6b21f78f285343
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-12-18 16:01:14 -0500

    redirect to root when visiting /2025

--- src/app/[locale]/2025/page.tsx
+++ src/app/[locale]/2025/page.tsx
@@ -1,67 +1,11 @@
 import { unstable_setRequestLocale } from 'next-intl/server'
-import Favicon from '@/app/favicon.ico'
-import BigOG from '@/app/opengraph-image.png'
-import { ComingSoon } from './coming-soon/sections'
-
+import { redirect } from 'next/navigation'
 type Props = {
 	params: Promise<{ locale: string }>
 }
 
-export async function generateMetadata(props: Props) {
-    const params = await props.params;
-    unstable_setRequestLocale(params.locale)
-    const siteUrl = `https://www.mtlbaljam.org/${params.locale}/2025`
-    if (params.locale === 'fr') {
-		return {
-			title: 'MTL BAL JAM 2025',
-			description: 'MTL BAL JAM 2025',
-			alternates: {
-				canonical: siteUrl,
-			},
-			icons: [{ rel: 'icon', url: Favicon.src }],
-			openGraph: {
-				images: [
-					{
-						url: BigOG.src,
-						alt: 'MTL BAL JAM logo',
-						width: 512,
-						height: 512,
-					},
-				],
-				title: 'MTL BAL JAM 2025',
-				locale: 'fr',
-				description: 'MTL BAL JAM 2025',
-			},
-		}
-	} else {
-		return {
-			title: 'MTL BAL JAM 2025',
-			description: 'MTL BAL JAM 2025',
-			alternates: {
-				canonical: siteUrl,
-			},
-			icons: [{ rel: 'icon', url: Favicon.src }],
-			openGraph: {
-				images: [
-					{
-						url: BigOG.src,
-						alt: 'MTL BAL JAM logo',
-						width: 512,
-						height: 512,
-					},
-				],
-				title: 'MTL BAL JAM 2025',
-				locale: 'en',
-				description: 'MTL BAL JAM 2025',
-			},
-		}
-	}
-}
-
-export default function Event2025() {
-	return (
-		<>
-			<ComingSoon />
-		</>
-	)
+export default async function Event2025({ params }: Props) {
+	const { locale } = await params
+	unstable_setRequestLocale(locale)
+	redirect(`/${locale}/`)
 }
