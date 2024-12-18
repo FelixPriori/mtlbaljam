10388979910524da544fa9e23625a16cf35a1818
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2024-12-18 16:00:50 -0500

    adds base metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react'
 
+export const metadata = {
+	metadataBase: new URL('https://mtlbaljam.org'),
+}
+
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return children
 }
