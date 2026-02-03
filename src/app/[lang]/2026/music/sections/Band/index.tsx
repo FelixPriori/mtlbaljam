11a0adfae820cc378e708bff11b742f52b7beb9f
FelixRiouxSabourin <rioux_sabourin.felix@courrier.uqam.ca>
2026-02-03 08:19:10 -0500

    adds second band to music page

--- src/app/[lang]/2026/music/sections/Band/index.tsx
+++ src/app/[lang]/2026/music/sections/Band/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './styles.module.scss'
 import { DictionaryType } from '@/app/[lang]/dictionaries'
+import { Fragment } from 'react/jsx-runtime'
 
 export default function Band({
 	liveMusic,
@@ -14,28 +15,32 @@ export default function Band({
 				<div className={styles.cutout}>
 					<Image
 						src="/michael-cutout.png"
-						alt={liveMusic.logoAlt}
+						alt={liveMusic.cutoutAlt}
 						width={402}
 						height={402}
 					/>
 				</div>
-				<div className={styles.logoWrapper}>
-					<Image
-						src="/legacy-band.png"
-						alt={liveMusic.logoAlt}
-						width={270}
-						height={291}
-					/>
-				</div>
-				<div className={styles.text}>
-					<h3>{liveMusic.bandName}</h3>
-					{liveMusic.biography.map(line => (
-						<p key={line}>{line}</p>
-					))}
-					<a target="_blank" rel="norefferer" href={liveMusic.link}>
-						{liveMusic.link}
-					</a>
-				</div>
+				{liveMusic.bands.map(band => (
+					<Fragment key={band.bandName}>
+						<div className={styles.logoWrapper}>
+							<Image
+								src={band.logoSrc}
+								alt={band.logoAlt}
+								width={1080}
+								height={1080}
+							/>
+						</div>
+						<div className={styles.text}>
+							<h3>{band.bandName}</h3>
+							{band.biography.map(line => (
+								<p key={line}>{line}</p>
+							))}
+							<a target="_blank" rel="norefferer" href={band.link}>
+								{band.link}
+							</a>
+						</div>
+					</Fragment>
+				))}
 			</div>
 		</section>
 	)
