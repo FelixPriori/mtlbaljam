7b43fe94ca00f6e323f35e5cdbbe9764234391f7
David Schlachter <t480-debian-git@schlachter.ca>
2026-02-20 17:03:23 +0100

    Auto-open relevant navigation tab

--- src/util/navigationUtils.ts
+++ src/util/navigationUtils.ts
@@ -1,3 +1,5 @@
+import { NavigationData } from '../app/[lang]/components/Navigation'
+
 export const checkIsCurrent = (page: string, current: string) => {
 	if (current === '/' && page === 'home') {
 		return true
@@ -24,4 +26,30 @@ export const getSlugFromPathname = (pathname: string) => {
 export const getPageNameFromSlug = (pathname: string) => {
 	const slug = getSlugFromPathname(pathname)
 	return slug.slice(1)
+}
+
+// When the user starts browsing, we need to figure out which navigation
+// section to have expanded. This applies only when the user loads a page
+// directly, coming from an external source.
+export const getDefaultToggledTab = (pathname: string, navigation: NavigationData) => {
+	// If the user is on the homepage, we should expand the navigation section
+	// for the most recent event (to make the details more discoverable).
+	if (pathname === "/en" || pathname === "/fr") {
+		return Object.keys(navigation).filter(
+			key => /\d{4}/.test(key)
+		).sort().reverse()[0] ?? null
+	}
+	// Otherwise, figure out which tab should be expanded, based on the current
+	// page.
+	for (const [key, value] of Object.entries(navigation)) {
+		if (typeof value === 'object' && 'subtabs' in value) {
+			for (const tab of value.subtabs) {
+				if (tab.href == pathname) {
+					return key
+				}
+			}
+		}
+	}
+	// For any other situation, don't expand any tab by default.
+	return null;
 }
